fb4874e0c2d2163b5cf468a591939a05f9a45534
NatanLycan <nathaniel981@gmail.com>
2016-11-03 18:51:24 -0600

    Base de datos de conocimientos

---
+++ pokemon/consultas_CabreraHerreraNathaniel.lisp
@@ -0,0 +1,88 @@
+;;==========================================================
+;;
+;;      Consultas a base de conocimieento
+;;
+;;      POKEMON
+;;
+;;      Funciones de consulta valida:
+;;              + exist
+;;              - d-exist
+;;              * 4all
+;;              / not-4all
+;;
+;;       Tipos de Consulta:
+;;              1. 
+;;
+;;==========================================================
+(defparameter *k* ()) ;;contendra el conocimiento
+
+(defun read-k (filename)
+    (with-open-file (stream filename)
+        (let (
+              (numrows (read stream))
+              )
+            (read-line stream nil nil)
+            (dotimes (row numrows)
+                (setq *k* (push (read stream nil nil) *k*))    
+            )
+            (setq *k* (reverse *k*))
+        )
+    )
+)
+;;(read-k "C:/Users/Nathaniel/Documents/GitHub/FIA_ejercicios/pokemon/knowledge.txt")
+
+
+;;============================================================
+;;
+;;  Fase1
+;;
+;;  Separa la consulta de entrada para su posterior analisis
+;;
+;;============================================================
+
+(defun fase1 (c)
+    (let ((funcion (first c)) (clase (second c)) (condiciones (third c)))
+        (format t "~% Funcion usada ~A ~%" funcion)
+        (format t "Clase solicitada ~A ~%" (rest clase))
+        (format t "Condiciones ~A ~% ~%" condiciones)
+        
+    )
+)
+
+
+;;============================================================
+;;
+;;  Consult
+;;
+;;  Esta funcion hace consultas a la base de conocimiento
+;;
+;;============================================================
+
+(defun consult ()
+    (let ((fin 0) (c ()))
+        (loop while (eq fin 0) do
+            (princ "Igrese el orden:")
+            (setq c (read))
+            (if (equal c nil)
+                ()
+                ;;Else
+                (progn
+                    ;;(print c)
+                    (fase1 c)
+                    ;;(format t "~%")
+                )
+            )  
+        )
+    )
+)
+
+;Ejemplo de consulta simple
+;(existe (clase . pokemon) ((num . [>700])))
+;(no-existe (clase . pokemon) ((num . [>700])))
+;(paratodo (clase . pokemon) ((num . [>700])))
+;(no-paratodo (clase . pokemon) ((num . [>700])))
+;Ejemplo de consulta compleja
+;(existe (clase . pokemon) (AND (TIPO1 . ACERO) (OR (TIPO2 . HADA) (NUM . [<600]))))
+;(paratodo (clase . pokemon) (AND (TIPO1 . ACERO) (OR (TIPO2 . HADA) (NUM . [<600]))))
+;(no-existe (clase . pokemon) (AND (TIPO1 . ACERO) (OR (TIPO2 . HADA) (NUM . [<600]))))
+;(no-paratodo (clase . pokemon) (AND (TIPO1 . ACERO) (OR (TIPO2 . HADA) (NUM . [<600]))))
