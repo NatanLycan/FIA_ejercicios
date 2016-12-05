c410cbf9a63e0aa6741f98d7459fdf90ddaab85f
NatanLycan <nathaniel981@gmail.com>
2016-12-05 16:48:53 -0600

    +++++

--- ProblemaZebra/zebraPuzzle2_CabreraHerreraNathaniel.lisp
+++ ProblemaZebra/zebraPuzzle2_CabreraHerreraNathaniel.lisp
@@ -1,21 +1,11 @@
 (defvar *zebra-bc* ())
 (setq *zebra-bc*
-    '(  ;;REGLAS
-      
-      (<- (casa ?id ?b ?a ?d ?e ?f) (posicion ?id))
-      (<- (casa ?idn ?an ?c ?d ?e ?f) (nacionalidad ?idn ?an))
-      (<- (casa ?idm ?b ?am ?d ?e ?f) (mascota ?idm ?am))
-      (<- (casa ?idc ?b ?a ?dc ?e ?f) (cigarro ?idc ?ac))
-      (<- (casa ?idb ?b ?a ?d ?eb ?f) (bebida ?idb ?ab))
-      (<- (casa ?idco ?b ?a ?d ?e ?aco) (color ?idco ?aco))
-        ;;HECHOS(casa pos nacionalidad mascota cigarro bebida color)
-      ;;----existen 5 casas
-      (<- (posicion 1));;1
-      (<- (posicion 2));;1
-      (<- (posicion 3));;1
-      (<- (posicion 4));;1
-      (<- (posicion 5));;1
-      ;;----las casas son contiguas
+    '(
+      (<- (posicion 1))
+      (<- (posicion 2))
+      (<- (posicion 3))
+      (<- (posicion 4))
+      (<- (posicion 5))
       (<- (junto 1 2));;1.5
       (<- (junto 2 1));;1.5
       (<- (junto 2 3));;1.5
@@ -24,22 +14,36 @@
       (<- (junto 3 4));;1.5
       (<- (junto 5 4));;1.5
       (<- (junto 4 5));;1.5
-      (<- (nacionalidad ?id Ingles) (color ?id Rojo));;2
-      (<- (nacionalidad ?id Español) (mascota ?id Perro));;3
-      (<- (color ?id Verde) (bebida ?id Cafe));;4
-      (<- (nacionalidad ?id Ukraniano) (bebida ?id Te));;5
-      (<- (color ?id1 Verde) (color ?id2 Blanco) (junto ?id1 ?id2));;6
-      (<- (cigarro ?id OldGold) (mascota ?id Caracol));;7
-      (<- (cigarro ?id Kools) (color ?id Amarillo));;8
-      (<- (bebida 3 Leche));;9
-      (<- (nacionalidad 1 Noruego));;10
-      (<- (cigarro ?c1 Chesterfields) (mascota ?c2 Zorro) (junto ?c1 ?c2));;11
-      (<- (cigarro ?c Kools) (mascota ?m Caballo) (junto ?c ?m));;12
-      (<- (cigarro ?id LuckyStrike) (bebida ?id Jugo));;13
-      (<- (nacionalidad ?id Japones) (cigarro ?id Parliaments));;14
-      (<- (nacionalidad ?n Noruego) (color ?c Azul) (junto ?n ?c));;15
+      (<- (casa 1 Noruego ?c ?d ?e ?f))
+      (<- (casa 2 ?b2 ?c2 ?d2 ?e2 Azul))
+      (<- (casa 3 ?b ?c ?d Leche ?f))
+      ;;(<- (casa 4 ?b ?c ?d ?e ?f))
+      ;;(<- (casa 5 ?b ?c ?d ?e ?f))
+      (<- (casa ?a Ingles ?c ?d ?e Rojo));;2
+      (<- (casa ?a Español Perro ?d ?e ?f));;3
+      (<- (casa ?a ?b ?c ?d Café Verde));;4
+      (<- (casa ?a Ukraniano ?c ?d Té ?f));;5
+      (<- (junto ?a1 ?a2)
+          (casa ?a1 ?b1 ?c1 ?d1 ?e1 Verde)
+          (casa ?a2 ?b2 ?c2 ?d2 ?e2 Blanco)
+          (posicion ?a2));;6
+      (<- (casa ?a ?b Caracol OldGold ?e ?f));;7
+      (<- (casa ?a ?b ?c Kools ?e Amarillo));;8
+      (<- (junto ?a1 ?a2)
+          (casa ?a1 ?b1 ?c1 Chesterfields ?e1 ?f1) 
+          (casa ?a2 ?b2 Zorro ?d2 ?e2 ?f2)
+          (posicion ?a2));;11
+      (<- (junto ?a1 ?a2)
+          (casa ?a1 ?b1 ?c1 Kools ?e1 ?f1) 
+          (casa ?a2 ?b2 Caballo ?d2 ?e2 ?f2)
+          (posicion ?a2));;12
+      (<- (casa ?a ?b ?c LuckyStrike Jugo ?f));;13
+      (<- (casa ?a Japones ?c Parliaments ?e ?f));;14
       
+   
+      ;;Pregunta
+      ;;(<- (casa ?a ?b ?c ?d Agua ?f));;agua
+      ;;(<- (casa ?a ?b Zebra ?d ?e ?f));;zebra
       ))
 
-;;(casa pos nacionalidad mascota cigarrillo bebida color)
-;;(WITH-KB *zebra-bc* (ASK '(casa ?a ?b ?c ?d ?e ?f)))
+(WITH-KB *zebra-bc* (ASK '(casa ?a ?b ?c ?d ?e ?f)))
--- ProblemaZebra/zebraPuzzle_CabreraHerreraNathaniel.lisp
+++ ProblemaZebra/zebraPuzzle_CabreraHerreraNathaniel.lisp
@@ -27,9 +27,7 @@
       (<- (casa 1 Noruego ?c1 ?d1 ?e1 ?f1));;1
       (<- (casa 2 ?b2 ?c2 ?d2 ?e2 Azul));;1
       (<- (casa 3 ?b3 ?c3 ?d3 Leche ?f3));;1
-      (<- (casa ?a ?b ?c ?d ?e ?f)
-          (junto ?a ?p)
-          (posicion ?a));;cada casa tiene una posicion y esta existe junto a otra
+      
       #|(<- (casa ?a1 ?b1 ?c1 ?d1 ?e1 ?f1)
           (posicion ?a1)
           (junto ?a1 ?a2)
@@ -50,20 +48,20 @@
       (<- (casa ?a ?b ?c ?d Café Verde));;4
       (<- (casa ?a Ukraniano ?c ?d Té ?f));;5
       (<- (casa ?a1 ?b1 ?c1 ?d1 ?e1 verde)
-          (casa ?a2 ?b2 ?c2 ?d2 ?e2 blanco)
           (junto ?a1 ?a2)
+          (casa ?a2 ?b2 ?c2 ?d2 ?e2 blanco)
           (posicion ?a1)
           (posicion ?a2));;6
       (<- (casa ?a ?b Caracol OldGold ?e ?f));;7
       (<- (casa ?a ?b ?c Kools ?e Amarillo));;8
       (<- (casa ?a1 ?b1 ?c1 Chesterfields ?e1 ?f1) 
-          (casa ?a2 ?b2 Zorro ?d2 ?e2 ?f2)
           (junto ?a1 ?a2)
+          (casa ?a2 ?b2 Zorro ?d2 ?e2 ?f2)
           (posicion ?a1)
           (posicion ?a2));;11
       (<- (casa ?a1 ?b1 ?c1 Kools ?e1 ?f1) 
-          (casa ?a2 ?b2 Caballo ?d2 ?e2 ?f2)
           (junto ?a1 ?a2)
+          (casa ?a2 ?b2 Caballo ?d2 ?e2 ?f2)
           (posicion ?a1)
           (posicion ?a2));;12
       (<- (casa ?a ?b ?c LuckyStrike Jugo ?f));;13
